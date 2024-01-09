fb2d86dc78955cac1a60e900dfc8f41d0f319998
cakehonolulu <cakehonolulu@users.noreply.github.com>
2024-01-09 17:50:36 +0000

    Publishing Site cakehonolulu/cakehonolulu.github.io at 2adf92c01c5ed3725b4003233bf662e38e6870b1 on Tue Jan  9 17:50:36 UTC 2024

--- bootcamp/UD30/Task3/scripts/codigo.js
+++
@@ -1,7 +0,0 @@
-let months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-
-let i = 0;
-
-for (i; i < months.length; i++) {
-    console.log(months[i]);
-}
