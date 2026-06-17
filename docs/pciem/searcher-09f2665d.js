4a10adab7f04ddf8cd3cea2bf334f5d6f326066a
cakehonolulu <cakehonolulu@users.noreply.github.com>
2026-06-17 09:40:11 +0000

    deploy: a6fd6ce3e130c5ba401c778e48d14fa843c04b05

--- docs/pciem/searcher-09f2665d.js
+++ docs/pciem/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d15b630e.js',
+                path_to_root + 'searchindex-afc19fd1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
